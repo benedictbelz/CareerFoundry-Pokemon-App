e62a8e0a420d20ab9b7882856c17530e211350cf
Benedict Belz <info@benedictbelz.eu>
2021-04-16 15:23:20 +0200

    1.5: JavaScript Functions (2) - Part 2

--- js/script.js
+++ js/script.js
@@ -1,27 +1,44 @@
-let pokemonList = [
-	{
-		name: "Charmander",
-		height: 0.6,
-		types: ["fire", "steel"],
-	},
-	{
-		name: "Bulbasaur",
-		height: 0.7,
-		types: ["grass", "poison"],
-	},
-	{
-		name: "Squirtle",
-		height: 0.5,
-		types: ["water", "steel"],
-	},
-	{
-		name: "Pikachu",
-		height: 0.4,
-		types: ["electric", "psychic"],
-	},
-];
+let pokemonRepository = (function () {
 
-pokemonList.forEach(function(e) {
+	let pokemonList = [
+		{
+			name: "Charmander",
+			height: 0.6,
+			types: ["fire", "steel"],
+		},
+		{
+			name: "Bulbasaur",
+			height: 0.7,
+			types: ["grass", "poison"],
+		},
+		{
+			name: "Squirtle",
+			height: 0.5,
+			types: ["water", "steel"],
+		},
+		{
+			name: "Pikachu",
+			height: 0.4,
+			types: ["electric", "psychic"],
+		},
+	];
+
+	function add(pokemon) {
+		pokemonList.push(pokemon);
+	}
+
+	function getAll() {
+		return pokemonList;
+	}
+
+	return {
+		add: add,
+		getAll: getAll
+	};
+
+})();
+
+pokemonRepository.getAll().forEach(function(e) {
 	
 	let note = "";
 
