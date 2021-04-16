fe38979ff83869ac3d41e8e1f75a6b4294a96904
Benedict Belz <info@benedictbelz.eu>
2021-04-16 12:48:26 +0200

    1.3: JavaScript Basics (2)

--- js/script.js
+++ js/script.js
@@ -20,3 +20,15 @@ let pokemonList = [
 		types: ["electric", "psychic"],
 	},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+	
+	let current = pokemonList[i];
+	let note = "";
+
+	if (current.height >= 0.7) {
+		note = " - Wow, that's big!"
+	}
+
+	document.write(current.name+" (height: "+current.height+")"+note+"\n");
+}
